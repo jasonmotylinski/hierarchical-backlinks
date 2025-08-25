7a37dc14dc295c9af5a16b4e8ef47b3f884834af
Jason Motylinski <jason@motylinski.com>
2025-08-25 18:51:00 -0500

    feature: save collapsed state when changing file focus

--- src/nav/collapseButton.ts
+++ src/nav/collapseButton.ts
@@ -1,12 +1,15 @@
 import { App, setIcon, Events } from "obsidian";
+import HierarchicalBacklinksPlugin from "../main";
 
 export class CollapseButton extends Events  {
     private app: App;
+    private plugin: HierarchicalBacklinksPlugin;
     private parent: Element;
     private button :HTMLDivElement;
-    constructor(app: App, parent: Element) {
+    constructor(app: App, plugin: HierarchicalBacklinksPlugin, parent: Element) {
         super();
         this.app=app;
+        this.plugin=plugin;
         this.parent=parent;
     }
 
@@ -14,9 +17,15 @@ export class CollapseButton extends Events  {
         this.button=this.parent.createDiv({cls: "clickable-icon nav-action-button"});
         setIcon( this.button, 'list');
 
+        if(this.plugin.settings.collapseButtonState){
+            this.button.addClass("is-active");   
+        }
+        this.trigger("collapse-click", null);
+
         this.button.addEventListener("click", (e)=>{ 
             this.button.classList.toggle('is-active');
             this.trigger("collapse-click", e);
+            this.plugin.saveData({collapseButtonState: this.isCollapsed()});
         });
     }
 
--- src/nav/navButtonsView.ts
+++ src/nav/navButtonsView.ts
@@ -1,20 +1,25 @@
 import { App } from "obsidian";
 import { CollapseButton } from "./collapseButton";
+import HierarchicalBacklinksPlugin from "../main";
 
 export class NavButtonsView {
     private app;
+    private plugin: HierarchicalBacklinksPlugin;
     private parent;
     public collapseButton: CollapseButton;
-    constructor(app: App, parent: Element) {
+    constructor(app: App, plugin: HierarchicalBacklinksPlugin, parent: Element) {
         this.app=app;
+        this.plugin=plugin;
         this.parent=parent;
     }
 
-    render(){
+    create(){
         const navButtonsContainer=this.parent.createDiv({cls:"nav-header"})
                                              .createDiv({cls: "nav-buttons-container"});
-        this.collapseButton=new CollapseButton(this.app, navButtonsContainer);
+        this.collapseButton=new CollapseButton(this.app, this.plugin,navButtonsContainer);
+    }
+
+    render(){
         this.collapseButton.render();
-        
     }
 }
